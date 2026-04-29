11c91a92938bf1bd1202bc1600e798343d096c28
aleja <aleja@DESKTOP-POH28G0>
2026-04-28 21:24:00 -0600

    para agregar_productoCatalog

--- src/main/java/interfaz/mobimedline_sistema/AgregarProductoController.java
+++ src/main/java/interfaz/mobimedline_sistema/AgregarProductoController.java
@@ -113,11 +113,6 @@ public class AgregarProductoController {
         }
         
         //Con esto se manda los recibido de agregar a producto a catalogobasdeproductos y de catalogo se muestre a el procuto nuevo
-        
-        
-        
- 
-        
         CatalogoProductosBase.setProductosBase(producto);//Para mostrar en catalogo se agregaa al base de catalogo de productos// Autor:Alejandro
         
         // Mostrar resumen
